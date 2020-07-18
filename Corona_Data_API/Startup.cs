9f3f8b53c505f6f91bb1e721c3085cf1e1432433
len <lenstandvoss@yahoo.de>
2020-07-18 14:01:07 +0200

    Added gzip compression to responses

--- Corona_Data_API/Startup.cs
+++ Corona_Data_API/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Corona_Data_API.DataManager;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,16 @@ namespace Corona_Data_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure Compression level
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
+
+            // Add Response compression services
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            });
+
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
                 builder.SetIsOriginAllowed(hostName => true)
@@ -50,6 +62,7 @@ namespace Corona_Data_API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseResponseCompression();
             app.UseCors("MyPolicy");
 
             app.UseSwagger();
